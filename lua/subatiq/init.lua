6c0f590bd527bb3b654b7c5d3d318b33f5192cc2
Vladimir Semenov <subatiq@gmail.com>
2024-12-12 16:41:02 +0300

    Initial

---
+++ lua/subatiq/init.lua
@@ -0,0 +1,38 @@
+require("subatiq.set")
+require("subatiq.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local SubatiqGroup = augroup('Subatiq', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = SubatiqGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+--- Set cursor to be | in insert mode
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+
+require("subatiq.packer")
